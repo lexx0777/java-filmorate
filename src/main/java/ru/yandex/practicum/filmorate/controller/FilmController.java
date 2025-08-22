39a0c0750597ed223ba8faa3d4c82e72f0fa00e4
lexx0777 <lexxei@yandex.ru>
2025-08-23 01:53:57 +0300

    sprint 11_ver6

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -1,89 +1,57 @@
 package ru.yandex.practicum.filmorate.controller;
 
 import jakarta.validation.Valid;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
+import ru.yandex.practicum.filmorate.response.SuccessResponse;
+import ru.yandex.practicum.filmorate.service.FilmService;
 
-import java.util.*;
+import java.util.Collection;
 
 @RestController
-@RequestMapping("/films")
-@Slf4j
+@RequestMapping(path = "/films")
+@RequiredArgsConstructor
 public class FilmController {
-    private final Map<Integer, Film> films = new HashMap<>();
+
+    private final FilmService filmService;
 
     @GetMapping
-    public Collection<Film> findAll() {
-        return films.values();
+    public Collection<Film> getFilms() {
+        return filmService.getAllFilms();
     }
 
-    @GetMapping("/{id}")
-    public Film findFilmById(@PathVariable int id) {
-        Film film = films.get(id);
-        if (film == null) {
-            throw new NotFoundException("Фильм с id " + id + " не найден");
-        }
-        return film;
+    @GetMapping("/{filmId}")
+    public Film getFilm(@PathVariable Long filmId) {
+        return filmService.getFilmById(filmId);
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Film addFilm(@Valid @RequestBody Film film) {
-        log.info("Добавление нового фильма: {}", film);
-        try {
-            // Explicit validation
-            if (film.getDescription() != null && film.getDescription().length() > 200) {
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
-                        "Максимальная длина описания — 200 символов");
-            }
-
-            //film.validate();
-            film.setId((long) getNextId());
-            films.put(film.getId().intValue(), film);
-            log.info("Фильм успешно добавлен: {}", film);
-            return film;
-        } catch (ValidationException e) {
-            log.warn("Ошибка валидации при добавлении фильма: {}", e.getMessage());
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, e.getMessage());
-        } catch (NullPointerException e) {
-            log.warn("Ошибка валидации: {}", e.getMessage());
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Дата релиза обязательна");
-        }
+        return filmService.createFilm(film);
     }
 
     @PutMapping
     public Film updateFilm(@Valid @RequestBody Film newFilm) {
-        log.info("Обновление фильма с ID {}: {}", newFilm.getId(), newFilm);
-        try {
-            if (films.containsKey(newFilm.getId())) {
-                Film oldFilm = films.get(newFilm.getId());
-                newFilm.validate();
-                oldFilm.setName(newFilm.getName());
-                oldFilm.setDescription(newFilm.getDescription());
-                oldFilm.setReleaseDate(newFilm.getReleaseDate());
-                oldFilm.setDuration(newFilm.getDuration());
-                log.info("Фильм успешно обновлен: {}", oldFilm);
-                return oldFilm;
-            }
-            throw new NotFoundException("Фильм с id = " + newFilm.getId() + " не найден");
-        } catch (ValidationException e) {
-            log.warn("Ошибка валидации при обновлении фильма: {}", e.getMessage());
-            throw e;
-        }
+        return filmService.updateFilm(newFilm);
     }
 
-    // вспомогательный метод для генерации нового id
-    private Integer getNextId() {
-        int currentMaxId = films.keySet()
-                .stream()
-                .mapToInt(id -> id)
-                .max()
-                .orElse(0);
-        return  ++currentMaxId;
+    @PutMapping("/{id}/like/{userId}")
+    public SuccessResponse likeFilm(@PathVariable Long id, @PathVariable Long userId) {
+        filmService.likeFilm(id, userId);
+        return new SuccessResponse();
+    }
+
+    @DeleteMapping("/{id}/like/{userId}")
+    public SuccessResponse removeLikeFilm(@PathVariable Long id, @PathVariable Long userId) {
+        filmService.removeLikeFilm(id, userId);
+        return new SuccessResponse();
+    }
+
+    @GetMapping("/popular")
+    public Collection<Film> getPopularFilms(@RequestParam(defaultValue = "10") int count) {
+        return filmService.getPopularFilms(count);
     }
 }
--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -1,114 +1,57 @@
 package ru.yandex.practicum.filmorate.controller;
 
-import jakarta.validation.*;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.server.ResponseStatusException;
-import ru.yandex.practicum.filmorate.exception.NotFoundException;
-import ru.yandex.practicum.filmorate.model.User;
 import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.response.SuccessResponse;
 import ru.yandex.practicum.filmorate.service.UserService;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Collection;
 
 @RestController
-@RequestMapping("/users")
-@Slf4j
+@RequestMapping(path = "/users")
 @RequiredArgsConstructor
 public class UserController {
-    private final Map<Integer, User> users = new HashMap<>();
+
     private final UserService userService;
 
     @GetMapping
-    public Collection<User> findAll() {
-        return users.values();
+    public Collection<User> getUsers() {
+        return userService.getAllUsers();
     }
 
-    @GetMapping("/{id}")
-    public User findUserById(@PathVariable int id) {
-        User user = users.get(id);
-        if (user == null) {
-            log.info("Пользователь с id " + id + " не найден");
-            throw new NotFoundException("Пользователь с id " + id + " не найден");
-        }
-        return user;
-    }
-
-    @DeleteMapping("/{id}")
-    public User deleteUserById(@PathVariable int id) {
-        User user = users.get(id);
-        if (user == null) {
-            log.info("Пользователь с id " + id + " не найден");
-            throw new NotFoundException("Пользователь с id " + id + " не найден");
-        }
-        return users.remove(id);
+    @GetMapping("/{userId}")
+    public User getFilm(@PathVariable Long userId) {
+        return userService.getUserById(userId);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public User createUser(@Valid @RequestBody User user) {
-        log.info("Создание нового пользователя: {}", user);
-        try {
-            Validator validator;
-            ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
-            validator = factory.getValidator();
-
-            // Validate the user
-            Set<ConstraintViolation<User>> violations = validator.validate(user);
-            if (!violations.isEmpty()) {
-                String errorMessage = violations.stream()
-                        .map(ConstraintViolation::getMessage)
-                        .collect(Collectors.joining(", "));
-                throw new ResponseStatusException(HttpStatus.BAD_REQUEST, errorMessage);
-            }
-            //user.validate();
-            user.setId((long) getNextId());
-            users.put(user.getId().intValue(), user);
-            log.info("Пользователь успешно создан: {}", user);
-            return user;
-        } catch (Exception e) {
-            log.error("Ошибка при создании пользователя: {}", e.getMessage(), e);
-            throw e;
-        }
+        return userService.createUser(user);
     }
 
     @PutMapping
     public User updateUser(@Valid @RequestBody User newUser) {
-        log.info("Обновление пользователя: {}", newUser);
-        try {
-            if (users.containsKey(newUser.getId().intValue())) {
-                User oldUser = users.get(newUser.getId().intValue());
-                // если user найден и все условия соблюдены, обновляем её содержимое
-                //todo проверить email и login на уникальность - потом
-                oldUser.setEmail(newUser.getEmail());
-                oldUser.setLogin(newUser.getLogin());
-                oldUser.setBirthday(newUser.getBirthday());
-                oldUser.setName(newUser.getName());
-                return oldUser;
-            }
-            throw new NotFoundException("Пользователь с id = " + newUser.getId() + " не найден");
-        } catch (Exception e) {
-            log.error("Ошибка при обновлении пользователя: {}", e.getMessage(), e);
-            throw e;
-        }
+        return userService.updateUser(newUser);
     }
 
     @PutMapping("/{id}/friends/{friendId}")
-    public Map<String, String> addFriend(@PathVariable Long id, @PathVariable Long friendId) {
+    public SuccessResponse addFriend(@PathVariable Long id, @PathVariable Long friendId) {
         userService.addFriend(id, friendId);
-        return Map.of("status", "success");
+        return new SuccessResponse();
     }
 
     @DeleteMapping("/{id}/friends/{friendId}")
-    public Map<String, String> removeFriend(@PathVariable Long id, @PathVariable Long friendId) {
+    public SuccessResponse removeFriend(@PathVariable Long id, @PathVariable Long friendId) {
         userService.removeFriend(id, friendId);
-        return Map.of("status", "success");
+        return new SuccessResponse();
     }
 
     @GetMapping("/{id}/friends")
-    public Collection<User> getUserFriends(@PathVariable Long id) {
+    public Collection<User> getFriends(@PathVariable Long id) {
         return userService.getUserFriends(id);
     }
 
@@ -117,13 +60,7 @@ public class UserController {
         return userService.getCommonUserFriends(id, otherId);
     }
 
-    // вспомогательный метод для генерации нового id
-    private Integer getNextId() {
-        int currentMaxId = users.keySet()
-                .stream()
-                .mapToInt(id -> id)
-                .max()
-                .orElse(0);
-        return  ++currentMaxId;
-    }
+
+
+
 }
